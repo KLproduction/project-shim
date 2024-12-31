d7a7bf884d315d76a9e5df738451c1d2f7843125
KentLaw <kent1988hk@gmail>
2024-12-31 15:32:25 +0000

    commit

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -8,8 +8,6 @@ const app = new Hono().basePath("/api");
 const routes = app.route("/auth", auth).route("/workspaces", workspaces);
 
 export const GET = handle(app);
-export const DELETE = handle(app);
-export const PATCH = handle(app);
 export const POST = handle(app);
 
 export type AppType = typeof routes;
